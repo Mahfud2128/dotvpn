34ae7ebcd8e18f46adec88e625c4e7276cae300b
scvpn <106874650+scvpn@users.noreply.github.com>
2022-06-05 22:03:29 +0800

    Update ext.sh

--- dll/ext.sh
+++ dll/ext.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /usr/bin/add-host "https://raw.githubusercontent.com/scvpn/dotvpn/main/ssh/add-host.sh" && chmod +x /usr/bin/add-host
-wget -q -O /usr/bin/about "https://raw.githubusercontent.com/scvpn/dotvpn/main/ssh/about.sh" && chmod +x /usr/bin/about
+# wget -q -O /usr/bin/add-host "https://raw.githubusercontent.com/scvpn/dotvpn/main/ssh/add-host.sh" && chmod +x /usr/bin/add-host
+# wget -q -O /usr/bin/about "https://raw.githubusercontent.com/scvpn/dotvpn/main/ssh/about.sh" && chmod +x /usr/bin/about
